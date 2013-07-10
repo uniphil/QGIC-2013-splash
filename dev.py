147d177250666681dea8656bf529150dfc6df1a0
Philip Schleihauf <uniphil@gmail.com>
2013-07-09 23:49:00 -0400

    basic theme

--- dev.py
+++ dev.py
@@ -1,6 +1,23 @@
+#!/usr/bin/env python2
+
 from qgic13 import app
+try:
+    from scss import Scss as S
+    import os, glob
+    def compile_scss():
+        scss_shlex = os.path.join(app.name, 'static', 'scss', '*.scss')
+        scss_files = glob.glob(scss_shlex)
+        scss = (S().compile(open(f).read()) for f in scss_files)
+        [open(f.replace('scss', 'css'), 'w').write(S().compile(open(f).read())) for f in scss_files]
+        return 'compiled some scss: {}'.format(', '.join(scss_files))
+
+except ImportError:
+    compile_scss = lambda: 'pyscss not found. not compiling css.'
+
 
 app.debug = True
 
+
 if __name__ == '__main__':
+    print(compile_scss())
     app.run()
--- qgic13/endpoints.py
+++ qgic13/endpoints.py
@@ -5,3 +5,8 @@ from qgic13 import app
 @app.route('/')
 def home():
     return render_template('base.html', content='hello world!')
+
+
+@app.route('/element-test')
+def el_test():
+    return render_template('element-test.html')
